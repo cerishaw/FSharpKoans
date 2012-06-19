1b3c3e9cd1fea66e2c91af759a651059b1c1fa12
Ceri Shaw <mrs.cshaw@gmail.com>
2012-06-19 08:42:46 +0100

    Fix AboutStrings asserts

--- FSharpKoans/AboutStrings.fs
+++ FSharpKoans/AboutStrings.fs
@@ -13,19 +13,19 @@ type ``about strings``() =
     member this.StringValue() =
         let message = "hello"
 
-        AssertEquality message __
+        AssertEquality message "hello"
 
     [<Koan>]
     member this.StringConcatValue() =
         let message = "hello " + "world"
 
-        AssertEquality message __
+        AssertEquality message "hello world"
 
     [<Koan>]
     member this.FormattingStringValues() =
         let message = sprintf "F# turns it to %d!" 11
 
-        AssertEquality message __
+        AssertEquality message "F# turns it to 11!"
 
         //NOTE: you can use printf to print to standard output
 
@@ -36,13 +36,13 @@ type ``about strings``() =
     member this.FormattingOtherTypes() =
         let message = sprintf "hello %s" "world"
 
-        AssertEquality message __
+        AssertEquality message "hello world"
 
     [<Koan>]
     member this.FormattingAnything() =
         let message = sprintf "Formatting other types is as easy as: %A" (1, 2, 3)
 
-        AssertEquality message __
+        AssertEquality message "Formatting other types is as easy as: (1, 2, 3)"
 
     (* NOTE: For all the %formatters that you can use with string formatting 
              see: http://msdn.microsoft.com/en-us/library/ee370560.aspx *)
@@ -55,7 +55,7 @@ type ``about strings``() =
                         expiali\
                         docious"
 
-        AssertEquality message __
+        AssertEquality message "supercalifragilisticexpialidocious"
 
     [<Koan>]
     member this.Multiline() =
@@ -66,7 +66,11 @@ type ``about strings``() =
                         lines"
 
         AssertEquality
-              message __
+              message "This
+                        is
+                        on
+                        five
+                        lines"
 
     [<Koan>]
     member this.ExtractValues() =
@@ -75,15 +79,18 @@ type ``about strings``() =
         let first = message.[0]
         let other = message.[4] 
 
-        AssertEquality first __
-        AssertEquality other __
+        AssertEquality first 'h'
+        AssertEquality other 'o'
 
     [<Koan>]
     member this.ApplyWhatYouLearned() =
         (* It's time to apply what you've learned so far. Fill in the function below to
            make the asserts pass *)
         let getFunFacts x =
-            __
+            let double = x * 2
+            let square = x * x
+            let message = sprintf "%d doubled is %d, and the square of %d is %d!" x double x square
+            message
 
         let funFactsAboutThree = getFunFacts 3
         let funFactsAboutSix = getFunFacts 6
