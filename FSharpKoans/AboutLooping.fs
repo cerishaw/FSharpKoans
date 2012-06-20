48cf93a914a6bbacf6d68a394fd0ba21df9f3bdf
Ceri Shaw <mrs.cshaw@gmail.com>
2012-06-20 08:39:18 +0100

    Fix AbourLooping asserts

--- FSharpKoans/AboutLooping.fs
+++ FSharpKoans/AboutLooping.fs
@@ -18,7 +18,7 @@ type ``about looping``() =
         for value in values do
             sum <- sum + value
 
-        AssertEquality sum __
+        AssertEquality sum 55
        
     [<Koan>]
     member this.LoopingWithExpressions() =
@@ -27,7 +27,7 @@ type ``about looping``() =
         for i = 1 to 5 do
             sum <- sum + i
 
-        AssertEquality sum __
+        AssertEquality sum 15
 
     [<Koan>]
     member this.LoopingWithWhile() =
@@ -36,7 +36,7 @@ type ``about looping``() =
         while sum < 10 do
             sum <- sum + sum
 
-        AssertEquality sum __
+        AssertEquality sum 16
 
     (* NOTE: While these looping constructs can come in handy from time to time,
              it's often better to use a more functional approach for looping
